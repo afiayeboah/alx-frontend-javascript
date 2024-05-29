dccec8ca8acb162d23f020c0861d4c5ec6453fcc
afiayeboah <afia.yakuamoah@gmail.com>
2024-05-29 17:26:33 +0000

    read

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+xport default function createIteratorObject(report) {
+  const myarray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
+  }
+
+  return myarray;
+}
